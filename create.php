6300bcc48838228f56e94d605501a549bd2ca831
Ariful <150324752+ariful93805@users.noreply.github.com>
2023-12-06 10:48:12 +0600

    ariful

--- create.php
+++ create.php
@@ -1,115 +1,116 @@
-<?php 
-
-include "config.php";
-  if (isset($_POST['submit'])) {
-    $first_name = $_POST['firstname'];
-    $last_name = $_POST['lastname'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $gender = $_POST['gender'];
-    $sql = "INSERT INTO `users`(`firstname`, `lastname`, `email`, `password`, `gender`) VALUES ('$first_name','$last_name','$email','$password','$gender')";
-    $result = $conn->query($sql);
-    if ($result == TRUE) {
-      echo "New record created successfully.";
-    }else{
-      echo "Error:". $sql . "<br>". $conn->error;
-    } 
-    $conn->close(); 
-  }
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$name = $address = $salary = "";
+$name_err = $address_err = $salary_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+    
+    // Validate address
+    $input_address = trim($_POST["address"]);
+    if(empty($input_address)){
+        $address_err = "Please enter an address.";     
+    } else{
+        $address = $input_address;
+    }
+    
+    // Validate salary
+    $input_salary = trim($_POST["salary"]);
+    if(empty($input_salary)){
+        $salary_err = "Please enter the salary amount.";     
+    } elseif(!ctype_digit($input_salary)){
+        $salary_err = "Please enter a positive integer value.";
+    } else{
+        $salary = $input_salary;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($address_err) && empty($salary_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO employees (name, address, salary) VALUES (?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_address, $param_salary);
+            
+            // Set parameters
+            $param_name = $name;
+            $param_address = $address;
+            $param_salary = $salary;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records created successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
 ?>
-
-
-
-<!doctype html>
+ 
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
-
-
-
-
-
-<div class="container "> 
-
-  <div class="card"> 
-
-  <div class="card">
-  <div class="card-header">
-  <div class="text-center  py-2 bg- "> <h1>SIGNUP FORM</h1></div>
-  </div>
-
-  
-
-
-
-
-
-  <div class="col-lg-6 m-auto "> 
-    <div class="card-body"> 
-
-  <form action="" method="POST">
-  <fieldset>
-  <div class="card">
-  <div class="card-header">
-  <div class="text-center  py-2 bg- "><legend class="text-center py-2">PERSONAL INFORMATION</legend> </div>
-  </div>
-    
-    
-
-    First Name:<br>
-    <input type="text" name="firstname" placeholder="First Name" class="form-control mb2">
-    <br>
-    Last Name:<br>
-    <input type="text" name="lastname"placeholder="Last Name" class="form-control mb2">
-    <br>
-    Email:<br>
-    <input type="email" name="email"placeholder="email" class="form-control mb2" >
-    <br>
-     Password:<br>
-    <input type="password" name="password"placeholder="password" class="form-control mb2">
-    <br>
-
-
-
-
-
-   
-
-
-
- <!-- <select class="form-select form-select-lg mb-3" aria-label="Large select example">
-        <option selected> Gender:</option>
-        <option value="1">Male</option>
-        <option value="2">Female</option>
-        
-  </select> -->
-
-    Gender:<br>
-    <input type="radio" name="gender" value="Male">Male
-    <input type="radio" name="gender" value="Female">Female
-    <br>
-
-
-    <button class="btn btn-success ">
-    <input type="submit" name="submit" value="submit"placeholder="submit" class="form-control mb2">
-    </button>
-  </fieldset>
-</form>
-
-      </div>
-   </div>
-  </div>
-</div>
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Create Record</h2>
+                    <p>Please fill this form and submit to add employee record to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+                            <span class="invalid-feedback"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Address</label>
+                            <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $address_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Salary</label>
+                            <input type="text" name="salary" class="form-control <?php echo (!empty($salary_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $salary; ?>">
+                            <span class="invalid-feedback"><?php echo $salary_err;?></span>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
 </html>
-
